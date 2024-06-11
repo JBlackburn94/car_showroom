fc0e99c44d0a59c123d624660fb1fb4c3dbe97a5
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-06-11 12:29:41 +0100

    adds the SearchBar and CustomFilter components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import { Hero } from "@/components";
+import { Hero, SearchBar, CustomFilter } from "@/components";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x p-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore our vehicles.</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
